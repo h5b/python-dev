f292f71d7172c524913798178ba659a3c014e1f1
Sebastian Trahm <basti@schleifi.com>
2014-09-29 21:15:45 +0200

    Function name should reflect its duty

--- kata/tests/test_euler_problem06.py
+++ kata/tests/test_euler_problem06.py
@@ -11,14 +11,14 @@ from kata.kata.euler_problem_06 import sum_of_squares
 
 class TestEulerProblem06:
 
-    def test_euler06_sum_of_squares_from_1_to_10(self):
+    def test_euler06_returns_385_for_sum_of_squares_from_1_to_10(self):
         result = sum_of_squares(10)
         assert_equal(385, result)
 
-    def test_euler06_square_of_sum_from_1_to_10(self):
+    def test_euler06_returns_3025_for_square_of_sum_from_1_to_10(self):
         result = square_of_sum(10)
         assert_equal(3025, result)
 
-    def test_euler06_difference_between_sum_and_square_from_1_to_10(self):
+    def test_euler06_returns_2640_for_difference_between_sum_and_square_from_1_to_10(self):
         result = difference_between_sum_and_square(10)
         assert_equal(2640, result)
